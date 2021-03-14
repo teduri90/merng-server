63246f780e6e49404cb96c8989421f4540c7adfa
teduri90 <50533261+teduri90@users.noreply.github.com>
2021-03-15 02:53:34 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_DB: 'mongodb+srv://teduri90:KWmeet1217!!@cluster0.jiucw.mongodb.net/recommended?retryWrites=true&w=majority',
+    SECRET_KEY: 'secret code that cannot be decoded'
+};
