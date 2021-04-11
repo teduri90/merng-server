f2cde18c4ed026e52f73177ac9fa9dc94849c219
teduri90 <50533261+teduri90@users.noreply.github.com>
2021-04-11 23:43:24 +0900

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_DB: 'mongodb+srv://teduri90:KWmeet1217!!@cluster0.jiucw.mongodb.net/recommended?retryWrites=true&w=majority',
-    SECRET_KEY: 'secret code that cannot be decoded'
-};
